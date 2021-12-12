4843879c6d9e689579e29adc8a64af64b6dc6b62
crocodilian <57382505+aa75017730@users.noreply.github.com>
2021-12-12 09:14:45 +0800

    documents_墨鱼手记

---
+++ JS_failarmy/documents_moyu.js
@@ -0,0 +1,54 @@
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+# > 023 樊登读书 （20201026） by ddgksf2013
+https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$) url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/documents.js
+hostname = license.pdfexpert.com
+*/
+
+var obj= {
+  "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+  "subscriptionExpirationIntent": "userCancelled",
+  "receiptStatus": "ok",
+  "subscriptionExpirationDate": "17:48 25/11/2099",
+  "inAppStates": [
+    {
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+      "subscriptionExpirationIntent": "userCancelled",
+      "receiptStatus": "ok",
+      "subscriptionExpirationDate": "17:48 25/11/2019",
+      "isEligibleForIntroPeriod": false,
+      "originalTransactionId": "20000625420102",
+      "productName": "subscription",
+      "isInBillingRetryPeriod": false,
+      "type": "subscription",
+      "subscriptionState": "active",
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "isInGracePeriod": false
+    },
+    {
+      "originalTransactionId": "0000",
+      "entitlements": [
+      ],
+      "type": "custom purchase",
+      "productId": "documents6-user"
+    }
+  ],
+  "isDocuments6User": true,
+  "isEligibleForIntroPeriod": false,
+  "originalTransactionId": "20000625420102",
+  "isEligibleFor": [
+    "winback"
+  ],
+  "isInBillingRetryPeriod": false,
+  "type": "subscription",
+  "inAppPurchased": [
+    "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user"
+  ],
+  "bundleId": "com.readdle.ReaddleDocsIPad",
+  "receiptId": 1530908572000,
+  "chargingPlatform": "iOS AppStore",
+  "subscriptionState": "active",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isInGracePeriod": false
+};
+$done({body: JSON.stringify(obj)});
