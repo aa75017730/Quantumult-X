f89095bd4e3421380a9467bc69b3e93085ac2e35
crocodilian <57382505+aa75017730@users.noreply.github.com>
2022-08-03 16:37:18 +0800

    石墨文档VIP

--- JS_failarmy/shimo.js
+++ JS_failarmy/shimo.js
@@ -1,7 +1,14 @@
 /*
 石墨文档解锁VIP
 https://shimo.im/lizard-api/users/me
-mitm= shimo.im
+
+QX
+# 石墨文档 vip
+^https:\//shimo.im\/lizard-api\/users\/me url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/shimo.js
+
+Surge 4.0+
+石墨文档 vip= type=http-response,pattern=^https:\//shimo.im\/lizard-api\/users\/me,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/shimo.js
+#hostname = shimo.im
 */
 
 let obj = JSON.parse($response.body);
